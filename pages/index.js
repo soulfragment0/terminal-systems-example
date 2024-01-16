7874beede6c72d813c64139376e7edcfd55f454e
soulfragment0 <soulfragment@proton.me>
2024-01-16 00:19:09 -0800

    inital setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+// index.js
+// import Link from "next/link";
+
+export default function Index() {
+  return (
+    <div>
+      <h1> Index </h1>
+      <a href="/about">
+        <a> About </a>
+        <a> Resources </a>
+      </a>
+    </div>
+  );
+}
